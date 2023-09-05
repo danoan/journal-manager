dfefef5d4d156255925a30ddf91b959b113cbdf5
Daniel Martins Antunes <danoan2008@gmail.com>
2023-09-05 21:16:49 +0200

    Add tox in the dev environment

--- dev/create-venv/create-venv.sh
+++ dev/create-venv/create-venv.sh
@@ -1,19 +1,19 @@
 #! /usr/bin/env bash
 
-SCRIPT_PATH="$( cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PROJECT_PATH="${SCRIPT_PATH%journal-manager*}journal-manager"
 
 INPUT_FOLDER="${SCRIPT_PATH}/input"
 OUTPUT_FOLDER="${SCRIPT_PATH}/output"
 mkdir -p "${OUTPUT_FOLDER}"
 
-pushd "${PROJECT_PATH}" > /dev/null
+pushd "${PROJECT_PATH}" >/dev/null
 
 python3 -m venv .venv
 source .venv/bin/activate
 
 pip install --upgrade pip
 pip install -e .
+pip install build tox
 
-popd > /dev/null
-
+popd >/dev/null
