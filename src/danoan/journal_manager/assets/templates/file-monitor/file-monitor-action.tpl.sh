f0b796ea08d2683c4bc5b994022aa4869c81e158
Daniel Martins Antunes <danoan2008@gmail.com>
2023-10-11 07:34:09 +0200

    Update file monitor code
    Instead of monitor the default location of journals folder
    only, we now monitor the set of all journal locations in the
    build.

--- src/danoan/journal_manager/assets/templates/file-monitor/file-monitor-action.tpl.sh
+++ src/danoan/journal_manager/assets/templates/file-monitor/file-monitor-action.tpl.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-SCRIPT_PATH="$( cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 INPUT_FOLDER="${SCRIPT_PATH}/input"
 OUTPUT_FOLDER="${SCRIPT_PATH}/output"
@@ -12,23 +12,35 @@ rm -rf "${TEMP_BUILD_FOLDER}"
 mkdir -p "${TEMP_BUILD_FOLDER}"
 
 MODIFIED_FILE="$1"
-JOURNALS_FILES_FOLDER="{{data.journals_files_folder}}"
+
+LIST_OF_MONITORED_FOLDERS=()
+{% for journal_files_folder in data.journals_files_folder %}
+LIST_OF_MONITORED_FOLDERS+=("{{journal_files_folder}}")
+{% endfor %}
+
+for FOLDER in "${LIST_OF_MONITORED_FOLDERS[@]}"; do
+	MODIFIED_FILE_PREFIX_REMOVED="${MODIFIED_FILE#${FOLDER}/}"
+
+	if [[ ! ${MODIFIED_FILE} = ${MODIFIED_FILE_PREFIX_REMOVED} ]]; then
+		JOURNAL_FOLDER_PATH="${FOLDER%*${MODIFIED_FILE_PREFIX_REMOVED}}"
+		JOURNAL_FOLDER_NAME="$(basename "${JOURNAL_FOLDER_PATH}")"
+
+		if [[ -n "${JOURNAL_FOLDER_NAME}" ]]; then
+			break
+		fi
+	fi
+done
+
 JOURNALS_SITE_FOLDER="{{data.journals_site_folder}}"
 
-MODIFIED_FILE_PREFIX_REMOVED="${MODIFIED_FILE#${JOURNALS_FILES_FOLDER}/}"
-JOURNAL_FOLDER_NAME="${MODIFIED_FILE_PREFIX_REMOVED%%/*}"
+if [ -n ${JOURNAL_FOLDER_NAME} ]; then
 
-if [ -n ${JOURNAL_FOLDER_NAME} ]
-then
+	JOURNAL_LOCATION_FOLDER="${JOURNAL_FOLDER_PATH}"
+	echo "Rebuilding: ${JOURNAL_LOCATION_FOLDER}"
 
-    JOURNAL_LOCATION_FOLDER="${JOURNALS_FILES_FOLDER}/${JOURNAL_FOLDER_NAME}"
-    echo "Rebuilding: ${JOURNAL_LOCATION_FOLDER}" 
-    
-    journal-manager b --ignore-safety-questions --do-not-build-index --build-location "${TEMP_BUILD_FOLDER}" --jl "${JOURNAL_LOCATION_FOLDER}"
-    
-    rm -rf "${JOURNALS_SITE_FOLDER}/${JOURNAL_FOLDER_NAME}"
-    mv -f "${TEMP_BUILD_FOLDER}/site/${JOURNAL_FOLDER_NAME}" "${JOURNALS_SITE_FOLDER}"
+	jm b --ignore-safety-questions --do-not-build-index --build-location "${TEMP_BUILD_FOLDER}" --jl "${JOURNAL_LOCATION_FOLDER}"
+
+	rm -rf "${JOURNALS_SITE_FOLDER}/${JOURNAL_FOLDER_NAME}"
+	mv -f "${TEMP_BUILD_FOLDER}/site/${JOURNAL_FOLDER_NAME}" "${JOURNALS_SITE_FOLDER}"
 
 fi
-
-
